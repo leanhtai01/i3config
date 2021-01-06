77a4e58153e2212996fbf52b988f67dedf4722f1
leanhtai01 <leanhtai01@gmail.com>
2021-01-06 09:06:25 +0700

    fix variable name

--- i3/disable_touchpad.sh
+++ i3/disable_touchpad.sh
@@ -5,7 +5,7 @@ set -e
 id=$(xinput list | grep -Eio 'TouchPad\s*id\=[0-9]{1,2}' | grep -Eio '[0-9]{1,2}')
 isEnabled=$(xinput list-props $id | grep 'Device Enabled' | awk '{print $4}')
 
-if [ $isEnable = 1 ]
+if [ $isEnabled = 1 ]
 then
     xinput disable $id
 fi
